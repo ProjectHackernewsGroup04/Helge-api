a099995cdac92e8534521b1c8ca78584ffe3a669
UsernameDiana <ms.strele@gmail.com>
2018-10-10 14:35:56 +0200

    Explain Rabbitmq usage

--- app/producer.py
+++ app/producer.py
@@ -6,14 +6,15 @@ url = 'amqp://pgkettyz:PIjFclgL5aB8c0ZP4OwWUZZ3DbgKEriY@raven.rmq.cloudamqp.com/
 connection = pika.BlockingConnection(pika.ConnectionParameters(url))
 channel = connection.channel()
 
-# Create a `hello` queue to which the message will be delivered.
+# Create a queue to which the message will be delivered.
 channel.queue_declare(queue='Helge-api-posts')
 
 
+# Using connection, posting to 'Helge-api-posts', serialized as JSON
 def post_to_queue(message):
     channel.basic_publish(exchange='',
                           routing_key='Helge-api-posts',
                           body=json.dumps(message),
                           properties=pika.BasicProperties(
-                             delivery_mode = 2, # make message persistent
+                             delivery_mode=2,  # make message persistent
                           ))
