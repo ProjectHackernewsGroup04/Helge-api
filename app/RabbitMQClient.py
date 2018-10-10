deda92a800c166ae5820d8208c7809448709d630
UsernameDiana <ms.strele@gmail.com>
2018-10-10 12:56:50 +0200

    Not using Rabbitmq class

--- app/RabbitMQClient.py
+++
@@ -1,9 +0,0 @@
-class RabbitMqClient(object):
-    __connection = None
-
-    def __init__(self, connection):
-        self.__conntection = connection
-
-    @staticmethod
-    def post(json_object):
-        return "Posted"
