37c098e3e97ed41ac48aa37902485999521b9337
Andreas Styltsvig <ansty93@gmail.com>
2018-10-16 12:25:29 +0200

    refactoring producer to use local rabbitmq

--- app/rabbitmq_producer.py
+++ app/rabbitmq_producer.py
@@ -1,15 +1,17 @@
 import pika
 import json
+import os
+import time
 
-# Establish a connection with RabbitMQ server.
-url = 'amqp://dkwttbup:6l_JoEG8FQQNFR-Imf-FWzs8H2avSPhe@lion.rmq.cloudamqp.com/dkwttbup'
-params = pika.URLParameters(url)
-#local = pika.ConnectionParameters('rabbitmq')
-connection = pika.BlockingConnection(params)
+url = os.environ.get('amqp://dkwttbup:6l_JoEG8FQQNFR-Imf-FWzs8H2avSPhe@lion.rmq.cloudamqp.com/dkwttbup', 'rabbitmq')
+time.sleep(10)
+#params = pika.URLParameters(url)
+local = pika.ConnectionParameters('rabbitmq')
+connection = pika.BlockingConnection(local)
 channel = connection.channel()
-
 # Create a queue to which the message will be delivered.
 channel.queue_declare(queue='Helge-api-posts')
+print(" [*] Producer is ready")
 
 
 # Using connection, posting to 'Helge-api-posts', serialized as JSON
