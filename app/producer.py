c5d868c86d7c8088ee20b52b783dda1f1da3b428
Mikkel Ziemer <mzhjensen@gmail.com>
2018-10-17 20:41:26 +0200

    Now correctly published to rabbitMQ

--- app/producer.py
+++ app/producer.py
@@ -1,20 +1,19 @@
 import pika
 import json
+import os
 # Establish a connection with RabbitMQ server.
-url = 'amqp://dkwttbup:6l_JoEG8FQQNFR-Imf-FWzs8H2avSPhe@lion.rmq.cloudamqp.com/dkwttbup'
+url = os.environ['CLOUDAMQP_URL']
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
-# Create a queue to which the message will be delivered.
-channel.queue_declare(queue='Helge-api-posts')
-
 
 # Using connection, posting to 'Helge-api-posts', serialized as JSON
 def post_to_queue(message):
-    channel.basic_publish(exchange='',
-                          routing_key='Helge-api-posts',
-                          body=json.dumps(message),
+    # Create a queue to which the message will be delivered.
+    channel.queue_declare(queue='helge-api-posts')
+    channel.basic_publish(exchange='', routing_key='helge-api-posts', body=json.dumps(message),
                           properties=pika.BasicProperties(
-                             delivery_mode=2,  # make message persistent
+                              content_type='text/plain',
+                              delivery_mode=2,  # make message persistent
                           ))
