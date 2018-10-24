496b43ac437a51584cc0e6a2cda94cfedc73018d
Mikkel Ziemer <mzhjensen@gmail.com>
2018-10-24 15:33:33 +0200

    Tried to fix producer

--- app/producer.py
+++ app/producer.py
@@ -5,16 +5,17 @@ import os
 url = os.environ['CLOUDAMQP_URL']
 print(url)
 params = pika.URLParameters(url)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
 
 
 # Using connection, posting to 'Helge-api-posts', serialized as JSON
 def post_to_queue(message):
     # Create a queue to which the message will be delivered.
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
     channel.queue_declare(queue='helge-api-posts', durable=True)
     channel.basic_publish(exchange='', routing_key='helge-api-posts', body=json.dumps(message),
                           properties=pika.BasicProperties(
-                              content_type='text/plain',
+                              content_type='application/json',
                               delivery_mode=2,  # make message persistent
                           ))
+    connection.close()
